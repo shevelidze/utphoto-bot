cd766fdb90f976338dd7f98cf12960b4bf3bbb71
Denys Shevel <denys.shevel16@gmail.com>
2022-03-27 11:52:29 +0300

    Finish bot

--- __main__.py
+++ __main__.py
@@ -1,9 +1,20 @@
 import os
-import urllib
-import numpy
+import math
+import random
+import tempfile
+import requests
 from dotenv import load_dotenv
 import telebot
-import cv2
+from PIL import Image
+
+TONES = [
+    lambda: '█',
+    lambda: '▓',
+    lambda: '▒',
+    lambda: '░',
+    lambda: '.'
+]
+MAX_IMAGE_SIZE = 30
 
 load_dotenv()
 bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
@@ -28,12 +39,31 @@ def reply_to_invalid_command(message):
 def send_UTF_art(message):
     file_id = message.photo[0].file_id
     photo_telegram_file = bot.get_file(file_id)
-    request = urllib.request.urlopen(
+    response = requests.get(
         f'https://api.telegram.org/file/bot{bot.token}/{photo_telegram_file.file_path}'
     )
-    array = numpy.asarray(bytearray(request.read()), dtype=numpy.uint8)
-    image = cv2.imdecode(array, cv2.IMREAD_GRAYSCALE)
-    print(type(image))
-    print(image.shape)
+    image_tempfile = tempfile.TemporaryFile()
+    image_tempfile.write(response.content)
+    image = Image.open(image_tempfile)
+    if image.height > image.width:
+        new_height = MAX_IMAGE_SIZE
+        new_width = math.ceil(MAX_IMAGE_SIZE * image.width / image.height)
+    else:
+        new_width = MAX_IMAGE_SIZE
+        new_height = math.ceil(MAX_IMAGE_SIZE * image.height / image.width)
+
+    image = image.resize((new_width, new_height))
+    result = '```'
+    for row in range(image.height):
+        for column in range(image.width):
+            chanels = image.getpixel((column, row))
+            gray = (max(chanels) + min(chanels)) / 2
+            level = math.ceil(gray / 255 * len(TONES)) - 1
+            result += TONES[level]() * 2
+
+        result += '\n'
+
+    result += '```'
+    bot.send_message(message.chat.id, result, parse_mode='MarkdownV2')
 
 bot.infinity_polling()
